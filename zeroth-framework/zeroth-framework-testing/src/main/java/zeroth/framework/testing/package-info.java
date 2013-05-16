23b054e589e7a7cc989e42ffcbc79cf2c062d93d
nilcy <nilcy@mac.com>
2013-05-16 23:03:01 +0900

    OPEN - task 1: 方向付けをおこなう。 https://github.com/nilcy/zeroth/issues/issues/issue/1
    Signed-off-by: nilcy <nilcy@mac.com>

---
+++ zeroth-framework/zeroth-framework-testing/src/main/java/zeroth/framework/testing/package-info.java
@@ -0,0 +1,9 @@
+// ========================================================================
+// Copyright (C) IMPULSE Project Team. All rights reserved.
+// GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007
+// http://www.gnu.org/licenses/agpl-3.0.txt
+// ========================================================================
+/**
+ * @author nilcy
+ */
+package zeroth.framework.testing;
