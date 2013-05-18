0b5abbc9dd95d92b3b3b75a2fa73a4a3a4088ff6
nilcy <nilcy@mac.com>
2013-05-18 15:18:26 +0900

    OPEN - task 1: 方向付けをおこなう。 https://github.com/nilcy/zeroth/issues/issues/issue/1
    Signed-off-by: nilcy <nilcy@mac.com>

---
+++ zeroth-framework/zeroth-framework-enterprise/src/test/java/zeroth/framework/enterprise/infra/persistence/EntityManagerFactory.java
@@ -0,0 +1,28 @@
+// ========================================================================
+// Copyright (C) zeroth Project Team. All rights reserved.
+// GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007
+// http://www.gnu.org/licenses/agpl-3.0.txt
+// ========================================================================
+package zeroth.framework.enterprise.infra.persistence;
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+/**
+ * エンティティマネージャのファクトリー
+ * @author nilcy
+ */
+public final class EntityManagerFactory {
+    /** 基礎的なエンティティマネージャ */
+    @Produces
+    @PrimaryEntityManager
+    @PersistenceContext(unitName = "primary")
+    private EntityManager primaryEntityManager;
+    /** 補助的なエンティティマネージャ */
+    @Produces
+    @SecondaryEntityManager
+    @PersistenceContext(unitName = "secondary")
+    private EntityManager secondaryEntityManager;
+    /** Constructor. */
+    public EntityManagerFactory() {
+    }
+}
