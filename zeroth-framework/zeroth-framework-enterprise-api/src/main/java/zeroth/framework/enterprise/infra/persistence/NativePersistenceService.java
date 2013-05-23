bc0df1cefb60f0aaeda00c3bde14de45abebd47e
nilcy <nilcy@mac.com>
2013-05-23 21:34:42 +0900

    OPEN - task 1: 方向付けをおこなう。 https://github.com/nilcy/zeroth/issues/issues/issue/1
    Signed-off-by: nilcy <nilcy@mac.com>

---
+++ zeroth-framework/zeroth-framework-enterprise-api/src/main/java/zeroth/framework/enterprise/infra/persistence/NativePersistenceService.java
@@ -0,0 +1,12 @@
+// ========================================================================
+// Copyright (C) zeroth Project Team. All rights reserved.
+// GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007
+// http://www.gnu.org/licenses/agpl-3.0.txt
+// ========================================================================
+package zeroth.framework.enterprise.infra.persistence;
+/**
+ * 原始永続サービス(JPA2/NativeQuery)
+ * @author nilcy
+ */
+public interface NativePersistenceService extends PersistenceService {
+}
---
+++ zeroth-framework/zeroth-framework-enterprise-api/src/main/java/zeroth/framework/enterprise/infra/persistence/StandardPersistenceService.java
@@ -0,0 +1,12 @@
+// ========================================================================
+// Copyright (C) zeroth Project Team. All rights reserved.
+// GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007
+// http://www.gnu.org/licenses/agpl-3.0.txt
+// ========================================================================
+package zeroth.framework.enterprise.infra.persistence;
+/**
+ * 標準永続サービス(JPA2/JPQL)
+ * @author nilcy
+ */
+public interface StandardPersistenceService extends PersistenceService {
+}
